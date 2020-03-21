a2e8df648edf4a1ceb27a9ec36f750ff732093f8
clownmonster <mk6386223@gmail.com>
2020-03-21 12:57:49 +0530

    Initial push

---
+++ CoronaApp.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask.templating import render_template
+from covid import Covid
+
+app = Flask(__name__)
+app.static_folder = 'static'
+covobj = Covid()
+
+
+@app.route('/')
+@app.route('/Home')
+def index():
+    counter = covobj.get_status_by_country_name('italy')
+    return render_template('index.html',confirmed= counter['confirmed'], active = counter['active'],
+                                        Recovered = counter['recovered'], Deaths = counter['deaths'])
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
---
+++ templates/covidtest.py
@@ -0,0 +1,8 @@
+from covid import Covid
+
+cv = Covid()
+#print(cv.get_data())
+
+#countries = cv.list_countries()
+Covid_Id_data = cv.get_status_by_country_name('india')
+print(Covid_Id_data['id'])
