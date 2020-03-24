e1b95269dcc059ad5a5fcbed81e7911506240ad9
ClownMonster <mk6386223@gmail.com>
2020-03-24 20:29:53 +0530

    2nd push

--- CoronaApp.py
+++ CoronaApp.py
@@ -5,14 +5,14 @@ from covid import Covid
 app = Flask(__name__)
 app.static_folder = 'static'
 covobj = Covid()
-data_req_country = "india"
+data_req_country = "germany"
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/Home', methods=['GET', 'POST'])
 def index():
     counter = covobj.get_status_by_country_name(data_req_country)
     return render_template('index.html',confirmed= counter['confirmed'], active = counter['active'],
-                                        Recovered = counter['recovered'], Deaths = counter['deaths'])
+                                        Recovered = counter['recovered'], Deaths = counter['deaths'], country = data_req_country )
 
 if __name__ == '__main__':
     app.run(debug = True)
